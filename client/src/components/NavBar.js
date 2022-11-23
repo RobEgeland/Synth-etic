e2508191aefdc257840d945a83d4a3731c565f8d
Robert Egeland <robegeland@yahoo.com>
2022-11-22 22:08:34 -0600

    adds easter egg

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,29 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useContext } from 'react'
 import { UserContext } from '../context/UserContext'
+import augh from '../static/augh.mp3'
 
 
 
 const NavBar = () => {
+  const [click, setClick] = useState(0)
+  
+  const audio = new Audio(augh)
   const {loggedIn, setLoggedIn, currentUser, setCurrentUser} = useContext(UserContext)
   // const [logIn, setLogIn] = loggedIn
   // const [current, setCurrentUser] = user
-  function handClick() {
+  function handLogOut() {
     fetch("/logout", {method: "DELETE"})
     setCurrentUser()
     setLoggedIn(false)
   }
+
+  function handleClick() {
+    setClick(() => click + 1)
+    if(click === 2) {
+      audio.play()
+    }
+
+  }
+  setTimeout(() => {
+    setClick(0)
+    if(audio.play){
+      audio.pause()
+    }
+  }, 4000)
 
   return (
     <div>
         <div className='navbar'>
             <a><NavLink to='/'>Home</NavLink></a>
             <a><NavLink to='/signup'>SignUp</NavLink></a>
-            <a>{ currentUser ? <NavLink onClick={handClick} to='/'>Log Out</NavLink> : <NavLink to='/login'>Log In</NavLink>}</a>
+            <a>{ currentUser ? <NavLink onClick={handLogOut} to='/'>Log Out</NavLink> : <NavLink to='/login'>Log In</NavLink>}</a>
             <a><NavLink to='/explore'>Explore</NavLink></a>
             <a><NavLink to='/my-profile'>Profile</NavLink></a>
-            <a className='navbar-right'><NavLink to='/ahhh'>Synth-etic</NavLink></a>
+            <a className='navbar-right'><NavLink onClick={handleClick} to='/ahhh'>Synth-etic</NavLink></a>
         </div>
     </div>
   )
