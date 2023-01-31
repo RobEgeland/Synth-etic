2bab2f16c4d76afcc6d63f7d97c80c21a2a3987e
Robert Egeland <robegeland@yahoo.com>
2023-01-30 21:07:50 -0600

    adds a few more css adjustments

--- client/src/components/Effects.js
+++ client/src/components/Effects.js
@@ -37,33 +37,34 @@ const Effects = ({
     }
     return (
         <div className='effects'>
-            <input value={soundName} type={"text"} placeholder={"Sound Name"} onBlur={() => setNameTyping(false)} onClick={() => setNameTyping(true)} onChange={(e) => {
+            <h1 class="Title">Sound Name</h1>
+            <input className='sound_name' value={soundName} type={"text"} placeholder={"Sound Name"} onBlur={() => setNameTyping(false)} onClick={() => setNameTyping(true)} onChange={(e) => {
                 setSoundName(e.target.value)
                 }}>
             </input>
             <div className='effects_inner'>
                 <div className='reverb'>
-                    <Knob className='reverb' textColor={"white"} step={0.1} size={75} min={0} max={1} value={reverb} onChange={(e) => setReverb(e.value)} />
+                    <Knob className='reverb' textColor={"white"} step={0.1} size={90} min={0} max={1} value={reverb} onChange={(e) => setReverb(e.value)} />
                     <h3 className='knob_label'>Reverb</h3>
                 </div>
                 <div className='phaser'>
-                    <Knob className='phaser' textColor={"white"} step={0.1} size={75} min={0} max={1} value={phaser} onChange={(e) => setPhaser(e.value)} />
+                    <Knob className='phaser' textColor={"white"} step={0.1} size={90} min={0} max={1} value={phaser} onChange={(e) => setPhaser(e.value)} />
                     <h3 className='knob_label'>Phaser</h3>
                 </div>
                 <div className='distortion'>
-                    <Knob className='distortion' textColor={"white"} step={0.1} size={75} min={0} max={1} value={distortion} onChange={(e) => setDistortion(e.value)} />
+                    <Knob className='distortion' textColor={"white"} step={0.1} size={90} min={0} max={1} value={distortion} onChange={(e) => setDistortion(e.value)} />
                     <h3 className='knob_label'>Distortion</h3>
                 </div>
                 <div className='bitcrusher'>
-                    <Knob className='bitcrusher' textColor={"white"} step={0.1} size={75} min={0} max={1} value={bitcrusher} onChange={(e) => setBitcrusher(e.value)} />
+                    <Knob className='bitcrusher' textColor={"white"} step={0.1} size={90} min={0} max={1} value={bitcrusher} onChange={(e) => setBitcrusher(e.value)} />
                     <h3 className='knob_label'>BitCrusher</h3>
                 </div>
                 <div className='delay'>
-                    <Knob className='delay' textColor={"white"} step={0.1} size={75} min={0} max={1} value={delay} onChange={(e) => setDelay(e.value)} />
+                    <Knob className='delay' textColor={"white"} step={0.1} size={90} min={0} max={1} value={delay} onChange={(e) => setDelay(e.value)} />
                     <h3 className='knob_label'>Delay</h3>
                 </div>
                 <div className='feedback'>
-                    <Knob className='feedback' textColor={"white"} step={0.1} size={75} min={0} max={1} value={feedback} onChange={(e) => setFeedback(e.value)} />
+                    <Knob className='feedback' textColor={"white"} step={0.1} size={90} min={0} max={1} value={feedback} onChange={(e) => setFeedback(e.value)} />
                     <h3 className='knob_label'>Feedback</h3>
                 </div>
             </div>
