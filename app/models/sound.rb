c5de76621d232ddac30a0e7a2f68dd580cb5e1a1
Robert Egeland <robegeland@yahoo.com>
2023-03-22 20:29:18 -0500

    adds sound name validation

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,5 +1,6 @@
 class Sound < ApplicationRecord
     validates :user_id, presence: true
+    validates :sound_name, presence: true
     belongs_to :user
     has_many :soundeffects
     has_many :effects, through: :soundeffects
